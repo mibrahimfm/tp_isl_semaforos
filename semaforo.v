f71b01852fbd38e72b2dc6bcd583e9cc95d69f0b
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2019-06-21 12:40:26 -0300

    Implementing semaforo module

--- semaforo.v
+++ semaforo.v
@@ -1,36 +1,110 @@
 module semaforo(input clk, input rst, input bt,
 	output reg [2:0] A, output reg [2:0] B);
 
-	reg[4:0] state;
-	localparam S0 = 5'b00001,
-						  S1 = 5'b00010,
-						  S2 = 5'b00100,
-						  S3 = 5'b01000,
-						  S4 = 5'b10000;
+	`define VERDE 8'd1
+	`define AMARELO 8'd3
+	`define VERMELHO 8'd2
+
+	localparam A0 = 3'b001,
+						  A1 = 3'b010,
+						  A2 = 3'b100,
+	localparam B0 = 3'b001,
+						  B1 = 3'b010,
+						  B2 = 3'b100,	
+						  
 
 	reg[3:0] count;
-
-	localparam SEC5 = 4'd5,
-						  SEC1 = 4'd1;
 
 	always @(posedge clk or posedge rst)
 				if(rst == 1)
 					begin
-						state <= S0;
+						A <= A0;
+
 						count <= 0;
 					end
 				else
-					case(state)
-						S0: if(count < SEC5)
-									begin
-										state <= S0;
-										count <= count + 1;
-									end
-								else
-									begin
-										state <= S1;
-										count <= 0;
-									end
+					if(bt == 1)
+						begin
+						case(A)
+							A0: if(count < VERDE)
+										begin
+											A <= A0;
+											B <= B0;
+											count <= count + 1;
+										end
+									else
+										begin
+											A <= A1;
+											B <= B1
+											count <= 0;
+										end
+							A1: if(count < AMARELO)
+										begin
+											A <= A1;
+											B <= B1;
+											count <= count + 1;
+										end
+									else
+										begin
+											A <= A2;
+											B <= B2;
+											count <= 0;
+										end								
+							S2: if(count < VERMELHO)
+										begin
+											A <= A2;
+											B  <= B2;
+											count <= count + 1;
+										end
+									else
+										begin
+											A <= A0;
+											B <= B0;
+											count <= 0;
+										end
+							default A <= A0; B <= B0;
+						endcase							
+						end
+					else
+						case(A)
+							A0: if(count < VERDE)
+										begin
+											A <= A0;
+											B <= B0;
+											count <= count + 1;
+										end
+									else
+										begin
+											A <= A1;
+											b <= B0;
+											count <= 0;
+										end
+							A1: if(count < AMARELO)
+										begin
+											A <= A1;
+											B <= B0;
+											count <= count + 1;
+										end
+									else
+										begin
+											A <= A2;
+											B <= B0;
+											count <= 0;
+										end								
+							S2: if(count < VERMELHO)
+										begin
+											A <= A2;
+											B  <= B0;
+											count <= count + 1;
+										end
+									else
+										begin
+											A <= A0;
+											B <= B0;
+											count <= 0;
+										end
+							
+							default A <= A0; B <= B0;
+						endcase
 
-	//Insira seu codigo aqui
 endmodule
