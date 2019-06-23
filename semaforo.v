a4250d84612c878ac005976d957d3c4af099adc0
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2019-06-23 18:45:54 -0300

    fixing constants

--- semaforo.v
+++ semaforo.v
@@ -1,10 +1,6 @@
 module semaforo(input clk, input rst, input bt,
 	output reg [2:0] A, output reg [2:0] B);
 
-	`define VERDE = 8'd1
-	`define AMARELO 8'd3
-	`define VERMELHO 8'd2
-
 	localparam A0 = 3'b001, A1 = 3'b010, A2 = 3'b100;
 	localparam B0 = 3'b001, B1 = 3'b010, B2 = 3'b100;	
 
@@ -21,7 +17,7 @@ module semaforo(input clk, input rst, input bt,
 					if(bt == 1)
 						begin
 						case(A)
-							A0: if(count < VERDE)
+							A0: if(count < `VERDE)
 										begin
 											A <= A0;
 											B <= B0;
@@ -33,7 +29,7 @@ module semaforo(input clk, input rst, input bt,
 											B <= B1;
 											count <= 0;
 										end
-							A1: if(count < AMARELO)
+							A1: if(count < `AMARELO)
 										begin
 											A <= A1;
 											B <= B1;
@@ -45,7 +41,7 @@ module semaforo(input clk, input rst, input bt,
 											B <= B2;
 											count <= 0;
 										end								
-							A2: if(count < VERMELHO)
+							A2: if(count < `VERMELHO)
 										begin
 											A <= A2;
 											B  <= B2;
@@ -67,7 +63,7 @@ module semaforo(input clk, input rst, input bt,
 					else
 						begin
 						case(A)
-							A0: if(count < VERDE)
+							A0: if(count < `VERDE)
 										begin
 											A <= A0;
 											B <= B0;
@@ -79,7 +75,7 @@ module semaforo(input clk, input rst, input bt,
 											B <= B0;
 											count <= 0;
 										end
-							A1: if(count < AMARELO)
+							A1: if(count < `AMARELO)
 										begin
 											A <= A1;
 											B <= B0;
@@ -91,7 +87,7 @@ module semaforo(input clk, input rst, input bt,
 											B <= B0;
 											count <= 0;
 										end								
-							A2: if(count < VERMELHO)
+							A2: if(count < `VERMELHO)
 										begin
 											A <= A2;
 											B  <= B0;
