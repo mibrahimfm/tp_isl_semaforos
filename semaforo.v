03ddc26ad4bd798ca8a33eeffac764d1136bbf31
Fernanda Rocha <42345598+fernandagonc@users.noreply.github.com>
2019-06-24 17:47:56 -0300

    fixing problems

--- semaforo.v
+++ semaforo.v
@@ -1,6 +1,10 @@
 module semaforo(input clk, input rst, input bt,
 	output reg [2:0] A, output reg [2:0] B);
 
+	`define VERDE 8'd1
+	`define AMARELO 8'd3
+	`define VERMELHO 8'd2
+
 	localparam A0 = 3'b001, A1 = 3'b010, A2 = 3'b100;
 	localparam B0 = 3'b001, B1 = 3'b010, B2 = 3'b100;	
 
@@ -10,7 +14,7 @@ module semaforo(input clk, input rst, input bt,
 				if(rst == 1)
 					begin
 						A <= A0;
-						B <= B0
+						B <= B0;
 						count <= 0;
 					end
 				else
